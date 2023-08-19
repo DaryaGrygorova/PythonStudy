ba7a5733fd2c235e1a74bb3c9ca272b143164634
DaryaGrygorova <grygorova.darya@gmail.com>
2023-08-19 17:06:13 +0300

    HW_2

---
+++ Lesson_2/task3.py
@@ -0,0 +1,36 @@
+# Task 3 - Using python as a calculator.
+# Make a program with 2 numbers saved in separate variables a and b, 
+# then print the result for each of the following: 
+#  - Addition
+#  - Subtraction
+#  - Division
+#  - Multiplication
+#  - Exponent (Power)
+#  - Modulus
+#  - Floor division
+
+a = 12
+b = 5
+
+print(f"a = {a}", f"b = {b}", sep=", ")     # a = 12, b = 5
+
+#Addition
+print("a + b =", a + b)     # a + b = 17
+
+#Subtraction
+print("a - b =", a - b)     # a - b = 7
+
+#Division
+print("a / b =", a / b)     # a / b = 2.4
+
+#Multiplication
+print("a * b =", a * b)     # a * b = 60
+
+#Exponent (Power)
+print("a ** b =", a ** b)     # a ** b = 248832
+
+#Modulus
+print(r"a % b =", a % b)     # a % b = 2
+
+#Floor division
+print("a // b =", a // b)     # a // b = 2
