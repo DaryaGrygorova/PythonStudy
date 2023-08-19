7e4a714b6afa48416ddf6fb3897d013fcdf0a055
DaryaGrygorova <grygorova.darya@gmail.com>
2023-08-19 17:12:24 +0300

    Add description to tasks

--- Lesson_1/task2.py
+++ Lesson_1/task2.py
@@ -1,3 +1,10 @@
+# Task 2
+# Create a python program named "task2", and use the built-in function 'print' in it several times. 
+# Try to pass "sep", "end" params and pass several parameters separated by commas. 
+# Also, provide a comment text above each print statement, mentioned above, 
+# with the expected output after execution of the particular print statement.
+
+
 # Hello, World!!!
 print('Hello, World!!!')
 
--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -1,3 +1,21 @@
+# Task 3
+# Write a program, which has two print statements to print the following text 
+# (capital letters "O" and "H", made from "#" symbols):
+# #########
+# #       #
+# #       #
+# #       #
+# #########
+
+# #       #
+# #       #
+# #########
+# #       #
+# #       #
+
+# Pay attention that usage of spaces is forbidden, as well as creating the whole result 
+# text string using """ """, use '\n' and '\t' symbols instead.
+
 symbol = '#'
 str_with_two_symbols = f'{symbol}\t{symbol}'
 
