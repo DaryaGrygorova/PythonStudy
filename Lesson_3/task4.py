f6855f569ab5ec56c960586f23a545b99131fb6b
DaryaGrygorova <grygorova.darya@gmail.com>
2023-09-02 13:26:32 +0300

    Refactor task4.py with black and pylint

--- Lesson_3/task4.py
+++ Lesson_3/task4.py
@@ -1,24 +1,22 @@
-# Task 4 - The name check.
-# Write a program that has a variable with your name stored (in lowercase) 
-# and then asks for your name as input. The program should check 
-# if your input is equal to the stored name even if the given name has 
-# another case, e.g., if your input is “Anton” and the stored name is “anton”, 
-# it should return True.
+"""Task 4 - The name check.
+Write a program that has a variable with your name stored (in lowercase)
+and then asks for your name as input. The program should check
+if your input is equal to the stored name even if the given name has
+another case, e.g., if your input is “Anton” and the stored name is “anton”,
+it should return True.
+"""
 
-print(
-    "This program checks if your name matches the saved one." r"'\x'" "\n" 
-    )
+print(r"This program checks if your name matches the saved one. For exit enter '\x'\n")
 
-NAME="darya"
+NAME = "darya"
 
 while True:
     user_name = input("Please enter your name: ")
 
-    if user_name == r'\x':
+    if user_name == r"\x":
         break
 
     if user_name.lower() == NAME:
-        print(
-            f'Hello, {NAME.capitalize()}! Nice to meet you!', end='\n\n') 
+        print(f"Hello, {NAME.capitalize()}! Nice to meet you!", end="\n\n")
     else:
-        print('Excuse me, do I know you?', end='\n\n') 
+        print("Excuse me, do I know you?", end="\n\n")
