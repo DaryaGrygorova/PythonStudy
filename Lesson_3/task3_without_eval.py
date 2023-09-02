91a51622a972fbd10b4a24ad0ceb9fb8a321fdcf
DaryaGrygorova <grygorova.darya@gmail.com>
2023-09-02 12:25:16 +0300

    Refactor task3_without_eval.py with black and pylint

--- Lesson_3/task3_without_eval.py
+++ Lesson_3/task3_without_eval.py
@@ -1,65 +1,64 @@
-# Task 3 - The math quiz program.
-# Write a program that asks the answer for a 
-# mathematical expression, checks whether the user is right or wrong,
-# and then responds with a message accordingly.
+""" Task 3 - The math quiz program.
+Write a program that asks the answer for a
+mathematical expression, checks whether the user is right or wrong,
+and then responds with a message accordingly.
+"""
 
 import random
 
-print("Welcome to math quiz!!! For exit enter " r"'\x'")
+print(r"Welcome to math quiz!!! For exit enter '\x'")
 
 COUNT_TOURS = 5
-count_questions = 0
-count_right_answers = 0
+COUNT_QUESTIONS = 0
+COUNT_RIGHT_ANSWERS = 0
+
 
 def create_expression():
-    a = random.randrange(1, 11)
-    b = random.randrange(1, 11)
-    operator = random.choice(['*','//', '+', '-'])
+    """"Function return expression and its result"""
+    operand_1 = random.randrange(1, 11)
+    operand_2 = random.randrange(1, 11)
+    operator = random.choice(["*", "//", "+", "-"])
 
-    math_expression = f'{a} {operator} {b}'
-    expression_result = ''
+    math_expression = f"{operand_1} {operator} {operand_2}"
+    expression_result = ""
 
     match operator:
-        case '*': 
-            expression_result = str(a * b)
-        case '//': 
-            expression_result = str(a // b)
-        case '+': 
-            expression_result = str(a + b)
-        case '-': 
-            expression_result = str(a - b)
+        case "*":
+            expression_result = str(operand_1 * operand_2)
+        case "//":
+            expression_result = str(operand_1 // operand_2)
+        case "+":
+            expression_result = str(operand_1 + operand_2)
+        case "-":
+            expression_result = str(operand_1 - operand_2)
 
-    return (math_expression, expression_result)
+    return math_expression, expression_result
 
 
-while count_questions < COUNT_TOURS:
-
+while COUNT_QUESTIONS < COUNT_TOURS:
     expression, right_answer = create_expression()
 
     answer = input(
-        'Please enter the answer for a mathematical expression '
-        f'({count_questions + 1}/{COUNT_TOURS}): \n'
-        f'{expression} = '
-        )
+        "Please enter the answer for a mathematical expression "
+        f"({COUNT_QUESTIONS + 1}/{COUNT_TOURS}): \n"
+        f"{expression} = "
+    )
 
-    if answer == r'\x':
+    if answer == r"\x":
         break
 
     if answer == right_answer:
-        print(
-            random.choice(['Right!', 'Yes!', 'Congrats!', 'Wonderful!']),
-            end='\n\n'
-            ) 
-        count_right_answers += 1
+        print(random.choice(["Right!", "Yes!", "Congrats!", "Wonderful!"]), end="\n\n")
+        COUNT_RIGHT_ANSWERS += 1
 
     else:
         print(
-            random.choice(['Sorry...', 'Oh, no...', 'Maybe another time...']),
-            f'Correct answer: {right_answer} ',
-            end='\n\n'
-            ) 
-    
-    count_questions += 1 
+            random.choice(["Sorry...", "Oh, no...", "Maybe another time..."]),
+            f"Correct answer: {right_answer} ",
+            end="\n\n",
+        )
+
+    COUNT_QUESTIONS += 1
 
 else:
-    print(f"That's all! You got {count_right_answers} point(s)!", end='\n\n')  
+    print(f"That's all! You got {COUNT_RIGHT_ANSWERS} point(s)!", end="\n\n")
