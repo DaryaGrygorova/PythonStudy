d8b703e7f03bb373395987a19a11ae5798c83b56
DaryaGrygorova <grygorova.darya@gmail.com>
2023-09-02 15:42:16 +0300

    Refactor tasks with black and pylint

--- Lessons_7/task1.py
+++ Lessons_7/task1.py
@@ -1,9 +1,13 @@
-# Task 1 - A simple function.
-# Create a simple function called favorite_movie, which takes a string
-# containing the name of your favorite movie. The function should then
-# print "My favorite movie is named {name}".
+"""
+Task 1 - A simple function.
+Create a simple function called favorite_movie, which takes a string
+containing the name of your favorite movie. The function should then
+print "My favorite movie is named {name}".
+"""
+
 
 def favorite_movie(name):
+    """Print "My favorite movie is named {name}"""
     print(f"My favorite movie is named '{name}'")
 
 
