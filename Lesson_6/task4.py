544568b9a21e50c4f87415275c575f6f60f708c3
DaryaGrygorova <grygorova.darya@gmail.com>
2023-09-02 13:52:17 +0300

    Refactor task4.py with black and pylint

--- Lesson_6/task4.py
+++ Lesson_6/task4.py
@@ -1,12 +1,10 @@
-# Task 4
-# Створити лист із днями тижня.
-# В один рядок (ну або як завжди) створити словник виду: {1: "Monday", 2:...
-# Також в один рядок або як вдасться створити зворотний словник {"Monday": 1,
+"""Task 4
+Створити лист із днями тижня.
+В один рядок (ну або як завжди) створити словник виду: {1: "Monday", 2:...
+Також в один рядок або як вдасться створити зворотний словник {"Monday": 1,
+"""
 
-days = [
-    'Monday', 'Tuesday', 'Wednesday', 'Thursday',
-    'Friday', 'Saturday', 'Sunday'
-]
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 first_week_dict = {(i + 1): day for i, day in enumerate(days)}
 print(first_week_dict)
