b9974bf0c22d9a7a772aa73e98e13df1a27d1eee
DaryaGrygorova <grygorova.darya@gmail.com>
2023-09-22 15:16:49 +0300

    Corrections after code review

--- Lesson_5/task1.py
+++ Lesson_5/task1.py
@@ -1,7 +1,7 @@
-# Task 1 - The greatest number
-# Write a Python program to get the largest number from a list of random numbers
-# with the length of 10
-# Constraints: use only while loop and random module to generate numbers
+"""Task 1 - The greatest number
+Write a Python program to get the largest number from a list of random numbers
+with the length of 10
+Constraints: use only while loop and random module to generate numbers"""
 
 import random
 
@@ -13,15 +13,13 @@ new_list = []
 
 while len(new_list) < LIST_LENGTH:
     new_list.append(random.randint(0, 100))
-else:
-    largest_num = new_list[0]
 
-    i = 0
-    while i < LIST_LENGTH - 1:
-        i += 1                          # елемент під індексом 0 не потребує порівняння
-        if new_list[i] > largest_num:
-            largest_num = new_list[i]
-        else:
-            continue
-    else:
-        print("List: ", new_list, '\n', 'Largest number: ', largest_num, sep='')
+largest_num = new_list[0]
+
+i = 0
+while i < LIST_LENGTH - 1:
+    i += 1                          # елемент під індексом 0 не потребує порівняння
+    if new_list[i] > largest_num:
+        largest_num = new_list[i]
+
+print("List: ", new_list, '\n', 'Largest number: ', largest_num, sep='')
--- Lesson_5/task2.py
+++ Lesson_5/task2.py
@@ -1,8 +1,8 @@
-# Task 2 - Exclusive common numbers.
-# Generate 2 lists with the length of 10 with random integers from 1 to 10,
-# and make a third list containing the common integers between the 2 initial
-# lists without any duplicates.
-# Constraints: use only while loop and random module to generate numbers
+"""Task 2 - Exclusive common numbers.
+Generate 2 lists with the length of 10 with random integers from 1 to 10,
+and make a third list containing the common integers between the 2 initial
+lists without any duplicates.
+Constraints: use only while loop and random module to generate numbers"""
 
 import random
 
@@ -14,8 +14,8 @@ third_list = []
 while len(first_list) < LIST_LENGTH:
     first_list.append(random.randint(0, 10))
     second_list.append(random.randint(0, 10))
-else:
-    third_list = list(set(first_list).intersection(set(second_list)))
+
+third_list = list(set(first_list).intersection(set(second_list)))
 
 # An alternative way to compare lists without set() and for loop
 # i = 0
