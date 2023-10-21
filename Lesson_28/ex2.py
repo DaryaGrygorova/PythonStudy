052ec61e422bfe7fcb68a9962fc0b855c0b18c58
DaryaGrygorova <grygorova.darya@gmail.com>
2023-10-21 20:52:25 +0300

    Add binary tree code to lesson 28

--- Lesson_28/ex2.py
+++ Lesson_28/ex2.py
@@ -1,56 +1,40 @@
-"""
-Hash algorithm properties:
-1. "Avalanche effect" - the tiniest change in incoming data changes the result completely
-2. The possibility of each resulting checksum (hash) is the same to any other
-3. You can not recreate the original contents from the checksum easily
-"""
-import ctypes
+"""Representation of a binary tree data structure"""
+
+class Node:
+    """This represents both root node and a tree at the same time"""
+
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data: int|float):
+        assert type(data) in (int, float)
+
+        if data < self.data:
+            if self.left is None:
+                self.left = Node(data)
+            else:
+                self.left.insert(data)
+        elif data > self.data:
+            if self.right is None:
+                self.right = Node(data)
+            else:
+                self.right.insert(data)
+
+    def print_self(self):
+        # offset = 20
+        # current_level = []
+        # print(' ' * offset, end='')
+        if self.left:
+            self.left.print_self()
+        print(self.data)
+        if self.right:
+            self.right.print_self()
 
 
-def my_hash(key: str):
-    factor_1 = len(key) or 1
-    factor_2 = sum(ord(symbol) for symbol in key)
-    factor_3 = sum(ord(symbol) for symbol in ctypes.cast(id(key), ctypes.py_object).value)
-
-    result = (factor_2**2)/(factor_1+factor_3)
-
-    return int(result%100)
-
-def my_hash_2(key_2: str):
-    result = 1
-    max_int = 32
-    for symbol in key_2:
-        result = (result * max_int + ord(symbol)) % 100
-    return result
-
-
-class MyDict:
-    def __init__(self):
-        self.storage = [[[None, None]]] * 100
-
-    def get_by_key(self, my_key):
-        index = my_hash(my_key)
-        result = self.storage[index]
-        if len(result) > 1:
-            for key, value in result:
-                if key == my_key:
-                    return value
-        return result[0][1]
-
-    def save_value_by_key(self, my_key, value):
-        index = my_hash(my_key)
-        result = self.storage[index]
-        if len(result) > 1:
-            for ind, val in enumerate(result):
-                if val.key == my_key:
-                    self.storage[index][ind][1] = value
-        self.storage[index] = [[my_key, value]]
-
-
-my_custom_dict = MyDict()
-
-my_custom_dict.save_value_by_key("one", 1)
-my_custom_dict.save_value_by_key("two", 2)
-my_custom_dict.save_value_by_key("two", 6)
-
-print(my_custom_dict.get_by_key('two'))
+root = Node(12)
+root.insert(6)
+root.insert(14)
+root.insert(3)
+root.print_self()
