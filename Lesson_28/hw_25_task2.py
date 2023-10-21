37ad23288bf9ade70402a9ab69cc699cc31dc55f
DaryaGrygorova <grygorova.darya@gmail.com>
2023-10-21 20:50:18 +0300

    Refactor task2 to homework for lesson 26

--- Lesson_28/hw_25_task2.py
+++ Lesson_28/hw_25_task2.py
@@ -1,18 +1,19 @@
 """Task 2 - Implement in (__contains__) and len (__len__) methods for HashTable"""
 
 
-class HashTable:
+class MyHashTable:
     """Implementation of HastTable"""
     def __init__(self, max_length=0):
         self.max_length = max_length or 100
-        self.storage = [[]] * self.max_length
+        self.storage = []
+        for _ in range(self.max_length):
+            self.storage.append([])
         self.len = 0
 
     @classmethod
-    def _hash(cls, key_str: str, hash_len=100):
-        """Returns Hash for key string"""
+    def _hash(cls, key_str: str, hash_len):
+        """Returns Hash for key string from 0 to 100 from 0 or to 1000"""
         hash_value = 17
-
         for char in key_str:
             hash_value = (hash_value * 31 + ord(char)) % hash_len
 
@@ -21,7 +22,7 @@ class HashTable:
     def _find_item_by_key(self, my_key, ):
         """Returns tuple (index, inner_index, item_key, item_value) if key exists in table
          or returns tuple (index, None, None, None)"""
-        index = HashTable._hash(my_key, self.max_length)
+        index = MyHashTable._hash(my_key, self.max_length)
         result = self.storage[index]
         for inner_index, (item_key, item_value) in enumerate(result):
             if item_key == my_key:
@@ -57,7 +58,11 @@ class HashTable:
 
     def items(self):
         """Returns list with items"""
-        return [[*nested_lists] for nested_lists in self.storage]
+        res = []
+        for nested_lists in self.storage:
+            for item in nested_lists:
+                res.append(item)
+        return res
 
     def table_update(self, other_table):
         """Append new items to Table"""
@@ -68,7 +73,7 @@ class HashTable:
         """Rehash and extend storage"""
         if self.len == self.max_length:
             self.max_length *= 2
-            new_table = HashTable(self.max_length)
+            new_table = MyHashTable(self.max_length)
             for item_key, item_value in self.items():
                 new_table.save_value_by_key(item_key, item_value)
             self.storage = new_table.storage
@@ -76,7 +81,6 @@ class HashTable:
 
     def __contains__(self, item):
         """Returns True if table contains key that equal item"""
-        # value in HashTable() -> __contains__(HashTable(), value)
         _, __, item_key, __ = self._find_item_by_key(item)
         if item_key:
             return True
@@ -86,29 +90,27 @@ class HashTable:
         return self.len
 
     def __getitem__(self, item_key):
-        index = HashTable._hash(item_key, self.max_length)
-        result = self.storage[index]
-        for key, value in result:
-            if item_key == key:
-                return value
+        _, __, item_key, item_value = self._find_item_by_key(item_key)
+        if item_key:
+            return item_value
         raise KeyError
 
     def __setitem__(self, item_key, new_value):
         self.save_value_by_key(item_key, new_value)
 
 
-my_table = HashTable()
+my_table = MyHashTable(10)
 my_table["key"] = "new value"
-print(my_table["key"])
+for num in range(1, 7):
+    if num == 3:
+        continue
+    my_table.save_value_by_key(f"key{num}", f"value{num}")
 
-my_table["key1"] = "value1"
-my_table["key2"] = "value2"
-my_table["key4"] = "value3"
-my_table["key4"] = "value4"
-my_table["key5"] = "value5"
-my_table["key6"] = "value6"
+assert ("key1" in my_table) is True
+assert ("key3" in my_table) is False
+assert len(my_table) == 6
 
-print("key1" in my_table)  # True
-print("key3" in my_table)  # False
-
-print(len(my_table))  # 7
+for num in range(7, 27):
+    my_table.save_value_by_key(f"key{num}", f"value{num}")
+assert len(my_table) == 26
+assert my_table["key"] == "new value"
